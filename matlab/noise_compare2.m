83ec1cebfb1e5224486b577bdfe69dc0622b3efb
Alex Ryahovsky <alexryahovsky@gmail.com>
2012-06-24 19:36:20 +0400

    изменения в ПЗ

---
+++ matlab/noise_compare2.m
@@ -0,0 +1,30 @@
+function err = noise_compare2(s,Dict,noise)
+   
+    s=truncword(s);
+    s=awgn(s,noise);
+    hashKeys = Dict.keys;
+    i=1;
+    while hashKeys.hasMoreElements
+
+            key=hashKeys.nextElement;
+            wordslist(i)=key;
+            mfcc_s=mfcc(s);
+            mfcc_e=Dict.get(key);
+           distancelist(i)=dtw(mfcc_s(:,1:end),mfcc_e(:,1:end)); 
+            i=i+1;
+    end
+
+
+    [m, k] = min(distancelist);
+    disp(10-k);
+
+%         figure(count+1)
+             bar(9:-1:0,distancelist);
+     set(gca,'XLim',[-1 10]);
+     title('');
+     xlabel('Слово');
+     ylabel('Расстояние');
+        
+end
+% disp(strcat('ошибок: ', num2str(10*err),'%'));
+%sound(Dict.get(num2str(10-k)),sr)
